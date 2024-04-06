f04e5f14cc9350b0c861207c3158bc7e6046bcab
kasheshjaiin <jkashish8055@gmail.com>
2024-04-06 13:55:55 -0400

    updating javascript

--- static/js/app.js
+++ static/js/app.js
@@ -24,8 +24,14 @@ function fetchData() {
   // Function to update dashboard elements based on selected sample
   function updateDashboard(sample, data) {
     // Fetch data for selected sample
-    var sampleData = data.samples.find(item => item.id === sample);
-    var metadata = data.metadata.find(item => item.id === parseInt(sample));
+    console.log("Data")
+    console.log(sample);
+    console.log(data);
+    var sampleData = data.samples.filter(item => item.id === sample);
+    var metadata = data.metadata.filter(item => item.id === parseInt(sample));
+    console.log("Filtered Data")
+    console.log(sampleData)
+    console.log(metadata)
   
     // Update demographic info
     updateDemographicInfo(metadata);
@@ -39,19 +45,23 @@ function fetchData() {
   
   // Function to update demographic info display
   function updateDemographicInfo(metadata) {
+    console.log("Updating Demographic Info...");
+    console.log(metadata);
     var metadataPanel = d3.select("#sample-metadata");
     metadataPanel.html("");
   
-    Object.entries(metadata).forEach(([key, value]) => {
+    Object.entries(metadata[0]).forEach(([key, value]) => {
       metadataPanel.append("p").text(`${key}: ${value}`);
     });
   }
   
   // Function to update bar chart
   function updateBarChart(sampleData) {
-    var otuIds = sampleData.otu_ids.slice(0, 10).reverse();
-    var sampleValues = sampleData.sample_values.slice(0, 10).reverse();
-    var otuLabels = sampleData.otu_labels.slice(0, 10).reverse();
+    console.log("Updating Bar Chart...");
+    console.log(sampleData)
+    var otuIds = sampleData[0].otu_ids.slice(0, 10).reverse();
+    var sampleValues = sampleData[0].sample_values.slice(0, 10).reverse();
+    var otuLabels = sampleData[0].otu_labels.slice(0, 10).reverse();
   
     var trace = {
       x: sampleValues,
@@ -75,14 +85,16 @@ function fetchData() {
   
   // Function to update bubble chart
   function updateBubbleChart(sampleData) {
+    console.log("Updating Bubble Chart...");
+    console.log(sampleData)
     var trace = {
-      x: sampleData.otu_ids,
-      y: sampleData.sample_values,
-      text: sampleData.otu_labels,
+      x: sampleData[0].otu_ids,
+      y: sampleData[0].sample_values,
+      text: sampleData[0].otu_labels,
       mode: "markers",
       marker: {
-        size: sampleData.sample_values,
-        color: sampleData.otu_ids
+        size: sampleData[0].sample_values,
+        color: sampleData[0].otu_ids
       }
     };
   
